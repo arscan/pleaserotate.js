3e99020428ab05f5ac1fd00081c23535c2f83427
Rob Scanlon <robscanlon@gmail.com>
2014-12-20 22:45:28 -0500

    Clean up some ids to be more consistent

--- pleaserotate.js
+++ pleaserotate.js
@@ -18,7 +18,7 @@
     };
 
     var cssRules = [
-       "#pleaserotate-icon { margin-left: 50px; width: 200px; animation: pleaserotate-frames ease 2s; animation-iteration-count: infinite; transform-origin: 50% 50%; -webkit-animation: pleaserotate-frames ease 2s; -webkit-animation-iteration-count: infinite; -webkit-transform-origin: 50% 50%; -moz-animation: pleaserotate-frames ease 2s; -moz-animation-iteration-count: infinite; -moz-transform-origin: 50% 50%; -ms-animation: pleaserotate-frames ease 2s; -ms-animation-iteration-count: infinite; -ms-transform-origin: 50% 50%; }",
+       "#pleaserotate-graphic { margin-left: 50px; width: 200px; animation: pleaserotateframes ease 2s; animation-iteration-count: infinite; transform-origin: 50% 50%; -webkit-animation: pleaserotateframes ease 2s; -webkit-animation-iteration-count: infinite; -webkit-transform-origin: 50% 50%; -moz-animation: pleaserotateframes ease 2s; -moz-animation-iteration-count: infinite; -moz-transform-origin: 50% 50%; -ms-animation: pleaserotateframes ease 2s; -ms-animation-iteration-count: infinite; -ms-transform-origin: 50% 50%; }",
         "#pleaserotate-backdrop { background-color: white; top: 0; left: 0; position: fixed; width: 100%; height: 100%; }",
         "#pleaserotate-container { width: 300px; position: absolute; top: 50%; left: 50%; margin-right: -50%; transform: translate(-50%, -50%); -webkit-transform: translate(-50%, -50%); }",
         "#pleaserotate-message { margin-top: 20px; font-size: 1.3em; text-align: center; font-family: Verdana, Geneva, sans-serif; text-transform: uppercase }",
@@ -26,7 +26,7 @@
     ];
     
     var cssKeyframeRules = [
-        "pleaserotate-frames{ 0% { transform:  rotate(0deg) ; -moz-transform:  rotate(0deg) ;-webkit-transform:  rotate(0deg) ;-ms-transform:  rotate(0deg) ;} 49% { transform:  rotate(90deg) ;-moz-transform:  rotate(90deg) ;-webkit-transform:  rotate(90deg) ; -ms-transform:  rotate(90deg) ;  } 100% { transform:  rotate(90deg) ;-moz-transform:  rotate(90deg) ;-webkit-transform:  rotate(90deg) ; -ms-transform:  rotate(90deg) ;  } }",
+        "pleaserotateframes{ 0% { transform:  rotate(0deg) ; -moz-transform:  rotate(0deg) ;-webkit-transform:  rotate(0deg) ;-ms-transform:  rotate(0deg) ;} 49% { transform:  rotate(90deg) ;-moz-transform:  rotate(90deg) ;-webkit-transform:  rotate(90deg) ; -ms-transform:  rotate(90deg) ;  } 100% { transform:  rotate(90deg) ;-moz-transform:  rotate(90deg) ;-webkit-transform:  rotate(90deg) ; -ms-transform:  rotate(90deg) ;  } }",
     ];
 
     /* private functions */
@@ -118,11 +118,11 @@
     function createPhoneSVG(){
         var svg = document.createElementNS('http://www.w3.org/2000/svg','svg');
         svg.setAttributeNS('http://www.w3.org/2000/xmlns/','xmlns:xlink','http://www.w3.org/1999/xlink');
-        svg.setAttribute('id','pleaserotate-icon');
+        svg.setAttribute('id','pleaserotate-graphic');
         svg.setAttribute('viewBox','0 0 250 250');
 
         var group = document.createElementNS('http://www.w3.org/2000/svg','g');
-        group.setAttribute('id','PhonePath');
+        group.setAttribute('id','pleaserotate-graphic-path');
 
         if(options.forcePortrait){
             group.setAttribute('transform','rotate(-90 125 125)');
